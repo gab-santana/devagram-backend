4f3c1021c2daa3b4fd8e19b86a4190135f3fc657
gab-santana <gabrielsantana100@hotmail.com>
2022-08-23 17:55:13 -0300

    Acrescido publicação

---
+++ pages/api/publicacao.ts
@@ -0,0 +1,57 @@
+import { NextApiResponse } from "next";
+import type { RespostaPadraoMsg } from "../../types/RespostaPadraoMsg";
+import nc from 'next-connect'
+import { uploadImagemCosmic, upload } from "../../services/uploadImagemCosmic";
+import { connectMongoDB } from "../../middlewares/connectMongoDB";
+import { validarTokenJWT } from "../../middlewares/validarTokenJWT";
+import { PublicacaoModel } from "../../models/PublicacaoModel";
+import { UsuarioModel } from "../../models/UsuarioModel";
+
+const handler = nc()
+  .use(upload.single('file'))
+  .post(async (req: any, res: NextApiResponse<RespostaPadraoMsg>) => {
+    try {
+      const { userId } = req.query
+      const usuario = await UsuarioModel.findById(userId)
+      if (!usuario) {
+        return res.status(400).json({ erro: 'Usuário não informado' })
+
+      }
+
+      if (!req || !req.body) {
+        return res.status(400).json({ erro: 'Parametros de entrada não informada' })
+
+      }
+      const { descricao } = req?.body
+
+      if (!descricao || descricao.length < 2) {
+        return res.status(400).json({ erro: 'Descrição não é válida' })
+      }
+
+      if (!req.file || !req.file.originalname) {
+        return res.status(400).json({ erro: 'A imagem é obrigatória' })
+      }
+
+      const image = await uploadImagemCosmic(req)
+      const publicacao = {
+        idUsuario: usuario._id,
+        descricao,
+        foto: image.media.url,
+        data: new Date()
+      }
+
+      await PublicacaoModel.create(publicacao)
+      return res.status(200).json({ msg: 'A publicação criada com sucesso' })
+    } catch (e) {
+      return res.status(400).json({ erro: 'Erro ao enviar nova publicação' })
+    }
+  })
+
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
+
+export default validarTokenJWT(connectMongoDB(handler))
