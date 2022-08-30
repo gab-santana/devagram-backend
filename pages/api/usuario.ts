285916ade57b8c7259f5067549bd632dc77e3d61
gab-santana <gabrielsantana100@hotmail.com>
2022-08-30 14:44:39 -0300

    Alteração de nome e avatar do usuário

--- pages/api/usuario.ts
+++ pages/api/usuario.ts
@@ -3,23 +3,69 @@ import { connectMongoDB } from "../../middlewares/connectMongoDB";
 import { validarTokenJWT } from "../../middlewares/validarTokenJWT";
 import type { RespostaPadraoMsg } from "../../types/RespostaPadraoMsg";
 import { UsuarioModel } from "../../models/UsuarioModel";
+import nc from "next-connect";
+import { upload, uploadImagemCosmic } from "../../services/uploadImagemCosmic";
 
+const handler = nc()
+  .use(upload.single('file'))
+  .put(async (req: any, res: NextApiResponse<RespostaPadraoMsg>) => {
+    try {
+      const { userId } = req?.query
+      const usuario = await UsuarioModel.findById(userId)
 
-const usuarioEndPoint = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg | any>) => {
+      if (!usuario) {
+        return res.status(400).json({ erro: 'Usuário não autenticado' })
+      }
 
-  try {
-    const { userId } = req?.query
+      const { nome } = req.body
+      if (nome && nome.length > 2) {
+        usuario.nome = nome
+      }
 
-    const usuario = await UsuarioModel.findById(userId)
-    usuario.senha = null
-    return res.status(200).json(usuario)
+      const { file } = req
+      if (file && file.originalmente) {
 
-  } catch (e) {
+        const image = await uploadImagemCosmic(req)
 
-    console.log(e);
+        if (image && image.media && image.media.url) {
+          usuario.avatar = image.media.url
+        }
+      }
+
+      await UsuarioModel
+        .findByIdAndUpdate({ _id: usuario }, usuario)
+
+      return res.status(200).json({ msg: 'Usuário alterado com sucesso!' })
+    } catch (e) {
+      console.log(e)
+    }
+    return res.status(400).json({ erro: 'Não foi possível alterar imagem.' })
   }
 
-  return res.status(400).json({ msg: 'Não foi possivel obter dados do usuário' })
+  )
+  .get(async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg | any>) => {
+
+    try {
+      const { userId } = req?.query
+
+      const usuario = await UsuarioModel.findById(userId)
+      usuario.senha = null
+      return res.status(200).json(usuario)
+
+    } catch (e) {
+
+      console.log(e);
+    }
+
+    return res.status(400).json({ msg: 'Não foi possivel obter dados do usuário' })
+  })
+
+
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
 }
 
-export default validarTokenJWT(connectMongoDB(usuarioEndPoint))
+export default validarTokenJWT(connectMongoDB(handler))
